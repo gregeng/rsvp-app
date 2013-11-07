bf8a910751e07f2e5382209f1b65b11d10768c9a
Greg Eng <greg.eng@gmail.com>
2013-11-07 09:59:01 -0500

    build out events resource

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,25 @@
 class EventsController < ApplicationController
 
   def new
+    @event = Event.new
   end
 
   def create
+    Event.create(event_params)
+    redirect_to(events_path)
   end
 
   def index
+    @events = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
+
+private
+  def event_params
+    params.require(:event).permit(:name)
+  end
 end
